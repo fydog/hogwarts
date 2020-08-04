a541867653a877bf3391d967b3c3eee21301847c
fydog <548981261@qq.com>
2020-08-05 00:22:19 +0800

    作业1

--- class1/homework1.py
+++ class1/homework1.py
@@ -1,18 +1,18 @@
 def game():
     my_hp = 1000
-    en_hp = 1000
+    em_hp = 1000
     my_power = 200
-    en_power = 199
+    em_power = 199
     
     while True:
-        my_hp = my_hp - en_power
+        my_hp = my_hp - em_power
         print(my_hp)
-        en_hp = en_hp - my_power
-        print(en_hp)
+        em_hp = em_hp - my_power
+        print(em_hp)
         if my_hp <= 0:
             print("i lose")
             break
-        elif en_hp <= 0:
+        elif em_hp <= 0:
             print("i win")
             break
 game()
