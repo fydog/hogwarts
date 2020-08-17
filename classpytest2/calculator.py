c341f905cf4354e5137c77f3a3361db2a048a453
fydog <548981261@qq.com>
2020-08-17 20:07:29 +0800

    ----

---
+++ classpytest2/calculator.py
@@ -0,0 +1,18 @@
+# 计算器代码块，+-*/四个方法
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a -b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
+
+    def dcount(self, a):
+        return a*0.8
+
