30c8172b6216de0e260dbf0b1fdfca49946d5ffa
realtalk1007 <548981261@qq.com>
2020-08-17 23:13:21 +0800

    作业4

--- classpytest2/testing/conftest.py
+++ classpytest2/testing/conftest.py
@@ -1,6 +1,11 @@
+import os
 from typing import List
-import sys
-sys.path.append('../..')
+import pytest
+# import sys
+import yaml
+from classpytest2.calculator import Calculator
+
+# sys.path.append('../..')
 
 # id设置中文
 def pytest_collection_modifyitems(
@@ -10,3 +15,27 @@ def pytest_collection_modifyitems(
         item.name = item.name.encode('utf-8').decode('unicode-escape')
         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
 
+@pytest.fixture(scope='class')
+def get_cal():
+    print("开始计算")
+    cal = Calculator()
+    yield cal
+    print("结束计算")
+
+
+# 读取测试数据
+def get_datas():
+    # mydatapath = os.path.dirname(__file__) + '/datas/cal.yml'
+    with open('../datas/cal.yml', encoding='utf-8') as f:
+        mydatas = yaml.safe_load(f)
+        adddatas = mydatas['add']['datas']
+        addids = mydatas['add']['myids']
+        divdatas = mydatas['div']['datas']
+        divids = mydatas['div']['myids']
+        dcountdatas= mydatas['dcount']['datas']
+        dcountids = mydatas['dcount']['myids']
+    return [adddatas, addids, divdatas, divids, dcountdatas, dcountids]
+
+@pytest.fixture(params=get_datas()[0], ids=get_datas()[1])
+def get_datas(request):
+    return request.param
--- classpytest2/testing/test_cal.py
+++ classpytest2/testing/test_cal.py
@@ -1,53 +1,27 @@
 import pytest
-import yaml
-
-
-from classpytest.demo.calculator import Calculator
-
-# 读取测试数据
-def get_datas():
-    with open('../datas/cal.yml', encoding='utf-8') as f:
-        mydatas = yaml.safe_load(f)
-        adddatas = mydatas['add']['datas']
-        addids = mydatas['add']['myids']
-        divdatas = mydatas['div']['datas']
-        divids = mydatas['div']['myids']
-        dcountdatas= mydatas['dcount']['datas']
-        dcountids = mydatas['dcount']['myids']
-    return [adddatas, addids, divdatas, divids, dcountdatas, dcountids]
-
+import sys
+sys.path.append('../..')
 # 文件名： test_开头
 # 类名： Test 开头
 # 方法名： test_ 开头
+
+
 class TestCal:
 
-    def setup_class(self):
-        self.cal = Calculator()
-        print("开始")
-    # 初始化
-
-    def teardown_class(self):
-        print("结束")
-
     @pytest.mark.add
-    @pytest.mark.parametrize('a, b, expect', get_datas()[0], ids=get_datas()[1])
-
-    def test_add(self, a, b, expect):
-        # cal = Calculator()
-        result = round(self.cal.add(a, b), 2)
-        assert expect == result
+    def test_add(self, get_datas, get_cal):
+        result = round(get_cal.add(get_datas[0],get_datas[1]), 2)
+        assert get_datas[2] == result
     # round()方法返回浮点数x的四舍五入值，round( x [, n]  )，x -- 数值表达式，n -- 数值表达式，表示从小数点位数。
 
-    @pytest.mark.div
-    @pytest.mark.parametrize('a, b, expect', get_datas()[2], ids=get_datas()[3])
-
-    def test_div(self, a, b, expect):
-        result = round(self.cal.div(a, b),2)
-        assert expect == result
-
-    @pytest.mark.dcount
-    @pytest.mark.parametrize('a, expect', get_datas()[4], ids=get_datas()[5])
-
-    def test_dcount(self,a ,expect):
-        result = self.cal.dcount(a)
-        assert expect == result
+    # @pytest.mark.div
+    # # @pytest.mark.parametrize('a, b, expect', get_datas()[2], ids=get_datas()[3])
+    # def test_div(self, get_datas, get_cal):
+    #     result = round(get_cal.div(get_datas[0],get_datas[1], 2)
+    #     assert get_datas[2] == result
+    #
+    # @pytest.mark.dcount
+    # # @pytest.mark.parametrize('a, expect', get_datas()[4], ids=get_datas()[5])
+    # def test_dcount(self,get_datas, get_cal):
+    #     result = get_cal.dcount(get_datas[0],get_datas[1])
+    #     assert get_datas[2] == result
